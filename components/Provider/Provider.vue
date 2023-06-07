a279a3a3c875d4c42cea1edec6c1d8441b3d0364
Diego Chavez <ingdiegochavez18@gmail.com>
2023-06-07 12:16:37 -0500

    feat: add-providers

---
+++ components/Provider/Provider.vue
@@ -0,0 +1,272 @@
+<template>
+  <div>
+    <v-card class="ma-5">
+      <div class="d-flex justify-between">
+        <v-tooltip bottom color="primary" class="">
+          <template #activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              v-bind="attrs"
+              fab
+              x-small
+              elevation="2"
+              @click="backList(true)"
+              v-on="on"
+              class="mx-5 my-5"
+            >
+              <v-icon>mdi mdi-arrow-left</v-icon>
+            </v-btn>
+          </template>
+          <span>Volver atras</span>
+        </v-tooltip>
+        <cardTitle :page="page" />
+      </div>
+      <v-divider />
+      <v-form ref="form" v-model="valid" fluid>
+        <v-card-text>
+          <v-card-subtitle
+            class="font-weight-bold"
+            :value="'Datos de ' + page.name"
+          />
+          <v-row>
+            <v-col class="col-6">
+              <v-text-field
+                v-model="nit"
+                dense
+                outlined
+                label="* NIT"
+                :rules="requiredField"
+              >
+                <template #prepend>
+                  <v-tooltip bottom color="primary">
+                    <template #activator="{ on }">
+                      <v-icon v-on="on"> mdi mdi-help-circle-outline </v-icon>
+                    </template>
+                    <span>NIT del Proveedor</span>
+                  </v-tooltip>
+                </template>
+              </v-text-field>
+            </v-col>
+
+            <v-col class="col-6">
+              <v-text-field
+                v-model="razonSocial"
+                dense
+                outlined
+                label="* Razon Social"
+                :rules="requiredField"
+              >
+                <template #prepend>
+                  <v-tooltip bottom color="primary">
+                    <template #activator="{ on }">
+                      <v-icon v-on="on"> mdi mdi-help-circle-outline </v-icon>
+                    </template>
+                    <span>Razon Social del Proveedor</span>
+                  </v-tooltip>
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col class="col-6">
+              <v-select
+                v-model="typeSelected"
+                dense
+                outlined
+                label="* Tipo"
+                :close-on-click="false"
+                :close-on-content-click="false"
+                item-value="id"
+                item-text="name"
+                :items="type"
+                :rules="requiredField"
+              >
+                <template #prepend>
+                  <v-tooltip bottom color="primary">
+                    <template #activator="{ on }">
+                      <v-icon v-on="on"> mdi mdi-help-circle-outline </v-icon>
+                    </template>
+                    <span>Tipo de Proveedor.</span>
+                  </v-tooltip>
+                </template>
+              </v-select>
+            </v-col>
+
+            <v-col class="col-6">
+              <v-select
+                v-model="statusSelected"
+                dense
+                outlined
+                label="Estado"
+                :close-on-click="false"
+                :close-on-content-click="false"
+                item-value="id"
+                item-text="name"
+                :items="status"
+                :rules="requiredField"
+              >
+                <template #prepend>
+                  <v-tooltip bottom color="primary">
+                    <template #activator="{ on }">
+                      <v-icon v-on="on"> mdi mdi-help-circle-outline </v-icon>
+                    </template>
+                    <span>Estado del Proveedor.</span>
+                  </v-tooltip>
+                </template>
+              </v-select>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-divider />
+        <v-card-actions class="px-3 pb-3">
+          <v-flex text-right>
+            <v-tooltip bottom color="primary">
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  small
+                  v-bind="attrs"
+                  elevation="2"
+                  v-on="on"
+                  @click="handleSubmit"
+                >
+                  <v-icon>mdi mdi-content-save</v-icon>
+                </v-btn>
+              </template>
+              <span>Guardar</span>
+            </v-tooltip>
+          </v-flex>
+        </v-card-actions>
+      </v-form>
+      <alerts v-if="showAlert" :text="textAlert" :type="typeAlert" />
+    </v-card>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import BackendService from '@/services/Services'
+import { requiredField } from '@/widgets/Validate'
+import alerts from '@/components/Shared/Alerts/Alerts'
+import { MESSAGE_TYPES } from '@/widgets/Messages'
+import cardTitle from '@/components/Shared/CardTitle/CardTitle'
+
+export default defineComponent({
+  name: 'ProviderComponent',
+  components: {
+    cardTitle,
+    alerts,
+  },
+  emits: ['backList'],
+
+  setup(context, { emit }) {
+    const page = ref({
+      name: 'Proveedores',
+      namePl: 'Proveedor',
+      description: 'Crear proveedor',
+      icon: 'mdi mdi-account-tie',
+    })
+
+    const valid = ref(true)
+    const backendService = ref(new BackendService())
+    const nit = ref('')
+    const razonSocial = ref('')
+    const type = ref([
+      {
+        id: 1,
+        name: 'Nacional',
+      },
+      {
+        id: 2,
+        name: 'Internacional',
+      },
+    ])
+    const status = ref([
+      {
+        id: 1,
+        name: 'Activo',
+      },
+      {
+        id: 2,
+        name: 'Inactivo',
+      },
+    ])
+    const typeSelected = ref('')
+    const statusSelected = ref('')
+    const textAlert = ref('')
+    const showAlert = ref(false)
+    const typeAlert = ref('')
+    const items = ref([])
+    const dialogProviders = ref(false)
+
+    const backList = (value) => {
+      emit('backList', value, null)
+    }
+
+    const handleSubmit = () => {
+      if (nit.value === '' || razonSocial.value === '') {
+        textAlert.value = 'Verifica que no existan campos obligatorios vacios.'
+        showAlert.value = true
+        typeAlert.value = MESSAGE_TYPES.ERROR
+      } else {
+        const data = {
+          nit: nit.value,
+          razon_social: razonSocial.value,
+          type: typeSelected.value,
+          active: statusSelected.value,
+        }
+
+        backendService.value
+          .addProvider(data)
+          .then((response) => {
+            emit('backList', false, 'create')
+          })
+          .catch((error) => {
+            showAlert.value = true
+            textAlert.value = error.response.data.message
+            typeAlert.value = MESSAGE_TYPES.ERROR
+          })
+      }
+    }
+
+    const closeDialogProviders = (event) => {
+      dialogProviders.value = false
+      nit.value = ''
+      razonSocial.value = ''
+      type.value = ''
+      status.value = ''
+    }
+
+    return {
+      page,
+      textAlert,
+      showAlert,
+      typeAlert,
+      dialogProviders,
+      items,
+      requiredField,
+      valid,
+      alerts,
+      nit,
+      razonSocial,
+      type,
+      status,
+      typeSelected,
+      statusSelected,
+      backList,
+      handleSubmit,
+      closeDialogProviders,
+    }
+  },
+})
+</script>
+<style>
+.card-search {
+  position: absolute !important;
+  z-index: 100;
+  border: 2px solid #0082eb !important;
+  margin-top: 0px !important;
+}
+</style>
---
+++ pages/providers.vue
@@ -0,0 +1,335 @@
+<template>
+  <div>
+    <loader v-if="loader" />
+    <div v-else>
+      <div v-if="viewList">
+        <v-card class="ma-5">
+          <cardTitle :page="page" />
+          <v-divider />
+          <v-card-text>
+            <div class="d-flex justify-end">
+              <v-tooltip bottom>
+                <template #activator="{ on, attrs }">
+                  <v-btn
+                    color="primary"
+                    dark
+                    v-bind="attrs"
+                    small
+                    elevation="2"
+                    v-on="on"
+                    @click="newProvider()"
+                  >
+                    <v-icon>mdi mdi-plus-box-multiple</v-icon>
+                  </v-btn>
+                </template>
+                <span>Nuevo Registro</span>
+              </v-tooltip>
+            </div>
+            <div class="mx-10">
+              <v-text-field
+                v-model="search"
+                v-debounce:700ms="getSearch"
+                dense
+                append-icon="mdi mdi-magnify"
+                label="Buscar Proveedor"
+                single-line
+                hide-details
+              />
+            </div>
+            <div v-if="items.length > 0">
+              <v-simple-table class="ma-5">
+                <template #default>
+                  <thead>
+                    <tr>
+                      <th scope="col" class="text-center">ITEM</th>
+                      <th scope="col" class="text-center">NOMBRE</th>
+                      <th scope="col" class="text-center">ACCIONES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(item, index) in items" :key="index">
+                      <td class="text-center">
+                        {{ item.id }}
+                      </td>
+                      <td class="text-center">
+                        {{ item.razon_social }}
+                      </td>
+                      <td class="text-center">
+                        <v-btn
+                          elevation="2"
+                          color="primary"
+                          class="cstm-button mx-2"
+                          small
+                          @click="editProvider(item)"
+                        >
+                          <v-icon> mdi-pencil-circle </v-icon>
+                        </v-btn>
+                        <v-btn
+                          elevation="2"
+                          color="error"
+                          small
+                          class="cstm-button mx-2"
+                          @click="openDialogDelete(item.id)"
+                        >
+                          <v-icon> mdi-delete-circle </v-icon>
+                        </v-btn>
+                      </td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+              <pagination
+                :items="linksPaginate"
+                :new-page="currentPage"
+                @getPagination="getPagination"
+              />
+            </div>
+            <div v-else class="pa-5 text-center">
+              <span class="font-weight-black">No existe registro.</span>
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div v-else>
+        <createProvider v-if="viewCreate" @backList="backList" />
+        <editProvider
+          v-if="viewUpdate"
+          :provider="providerSelected"
+          @backList="backList"
+        />
+      </div>
+
+      <sheetAlerts
+        v-if="sheetOpenDelete"
+        :id="idSelected"
+        :name="page.namePl"
+        :icon="page.icon"
+        :sheet="sheetOpenDelete"
+        @sheetClosed="sheetClose"
+        @sheetDelete="sheetDelete"
+      />
+
+      <alerts v-if="showAlert" :text="textAlert" :type="typeAlert" />
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent, ref, onMounted } from '@nuxtjs/composition-api'
+import BackendService from '@/services/Services'
+import alerts from '@/components/Shared/Alerts/Alerts'
+import { MESSAGE_TYPES } from '@/widgets/Messages'
+import sheetAlerts from '@/components/Shared/SheetAlerts/SheetAlerts'
+import loader from '@/components/Shared/Loader/Loader'
+import pagination from '@/components/Shared/Pagination/Pagination'
+import cardTitle from '@/components/Shared/CardTitle/CardTitle'
+import createProvider from '@/components/Provider/Provider'
+import editProvider from '@/components/Provider/editProvider'
+
+export default defineComponent({
+  name: 'ChargesPage',
+  components: {
+    alerts,
+    sheetAlerts,
+    loader,
+    pagination,
+    cardTitle,
+    createProvider,
+    editProvider,
+  },
+  setup() {
+    const page = ref({
+      name: 'Proveedores',
+      namePl: 'Proveedor',
+      description: 'Listado de proveedores',
+      icon: 'mdi mdi-account-tie',
+    })
+
+    const backendService = ref(new BackendService())
+    const viewList = ref(true)
+    const viewCreate = ref(false)
+    const viewUpdate = ref(false)
+    const dialogDelete = ref(false)
+    const textAlert = ref('')
+    const showAlert = ref(false)
+    const typeAlert = ref('')
+    const search = ref('')
+    const items = ref([])
+    const idSelected = ref('')
+    const providerSelected = ref()
+    const loader = ref(false)
+    const linksPaginate = ref([])
+    const pageSelected = ref(1)
+    const totalPages = ref(0)
+    const countStart = ref(0)
+    const countEnd = ref(0)
+    const count = ref(1)
+    const sheetOpenDelete = ref(false)
+    const currentPage = ref(1)
+
+    /** METHODS */
+    onMounted(() => {
+      setTimeout(() => {}, 4000)
+      getProviders(0)
+    })
+
+    const newProvider = () => {
+      viewList.value = false
+      viewCreate.value = true
+      viewUpdate.value = false
+      showAlert.value = false
+    }
+
+    const editProvider = (item) => {
+      providerSelected.value = item
+      viewList.value = false
+      viewCreate.value = false
+      viewUpdate.value = true
+      showAlert.value = false
+    }
+
+    const backList = (value, status) => {
+      viewList.value = true
+      viewCreate.value = false
+      if (status === 'update') {
+        showAlert.value = true
+        textAlert.value = 'Proveedor actualizado con éxito.'
+        typeAlert.value = MESSAGE_TYPES.INFO
+        setTimeout(() => {
+          showAlert.value = false
+        }, 30000)
+      } else if (status === 'create') {
+        setTimeout(() => {
+          showAlert.value = false
+        }, 30000)
+        showAlert.value = true
+        textAlert.value = 'Proveedor registrado con éxito.'
+        typeAlert.value = MESSAGE_TYPES.INFO
+      }
+      getProviders()
+    }
+
+    const getProviders = (page = 0, search = '') => {
+      loader.value = true
+      const dataSearch = {
+        search,
+        page,
+      }
+
+      backendService.value
+        .getProviders(dataSearch)
+        .then((data) => {
+          items.value = []
+          page = data.data.current_page
+          currentPage.value = page
+
+          const countItems = data.data.per_page
+          countStart.value = countItems * page - countItems
+          countEnd.value = data.data.to - 1
+
+          for (let i = 0; i < data.data.data.length; i++) {
+            items.value.push(data.data.data[i])
+          }
+          linksPaginate.value = data.data.links
+          totalPages.value = data.data.last_page
+
+          if (count.value === 0) {
+            count.value = count.value + 1
+          }
+        })
+        .catch((error) => {
+          if (error.response.data.status >= 400) {
+            showAlert.value = true
+            textAlert.value = error.response.data.message
+            typeAlert.value = MESSAGE_TYPES.ERROR
+          }
+        })
+        .finally(() => {
+          loader.value = false
+          setTimeout(() => {
+            showAlert.value = false
+          }, 3000)
+        })
+    }
+
+    const getPagination = (url) => {
+      const link = new URL(url)
+      const page = link.searchParams.get('page')
+      getProviders(page)
+    }
+
+    const getSearch = () => {
+      getProviders(0, search.value)
+    }
+
+    const openDialogDelete = (value) => {
+      idSelected.value = ref(value)
+      sheetOpenDelete.value = true
+    }
+
+    const sheetClose = () => {
+      sheetOpenDelete.value = false
+    }
+
+    const sheetDelete = (value, id) => {
+      backendService.value
+        .deleteProvider(id)
+        .then((data) => {
+          idSelected.value = ''
+          items.value = []
+          sheetOpenDelete.value = false
+          getProviders(0)
+          showAlert.value = true
+          textAlert.value = 'Proveedor eliminado con éxito.'
+          typeAlert.value = MESSAGE_TYPES.INFO
+        })
+        .catch((error) => {
+          sheetOpenDelete.value = false
+          showAlert.value = true
+          textAlert.value = error.response.data.message
+          typeAlert.value = MESSAGE_TYPES.ERROR
+        })
+        .finally(() => {
+          setTimeout(() => {
+            showAlert.value = false
+          }, 3000)
+        })
+    }
+
+    return {
+      page,
+      backendService,
+      items,
+      viewList,
+      viewCreate,
+      viewUpdate,
+      showAlert,
+      textAlert,
+      typeAlert,
+      dialogDelete,
+      search,
+      idSelected,
+      providerSelected,
+      linksPaginate,
+      pageSelected,
+      sheetOpenDelete,
+      loader,
+      currentPage,
+      newProvider,
+      editProvider,
+      backList,
+      getProviders,
+      getPagination,
+      getSearch,
+      openDialogDelete,
+      sheetClose,
+      sheetDelete,
+    }
+  },
+})
+</script>
+<style>
+.subtitle {
+  font-size: 0.8rem;
+  font-style: normal !important;
+}
+</style>
